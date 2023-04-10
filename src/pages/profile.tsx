a059ee47461f1d1669669ef4e49b08093e9f1ac7
Cory Sougstad <cory@hau.me>
2023-04-10 14:20:31 -0700

    Add placeholder for profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+export const ProfilePage: NextPage = () => {
+  return (
+    <section className="flex h-full items-center justify-center">
+      <span className="text-3xl">Coming soon.</span>
+    </section>
+  );
+};
+
+export default ProfilePage;
